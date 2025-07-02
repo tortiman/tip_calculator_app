65e88759ab20b17eae9c8b01291fc3bd8c467449
tortiman <josejavierbedmar@gmail.com>
2025-07-02 18:43:14 +0200

    responsive design

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const moneyId=document.getElementById('moneyId');
+    const _5Percent=document.getElementById('5Id');
+    const _10Percent=document.getElementById('10Id');
+    const _15Percent=document.getElementById('15Id');
+    const _25Percent=document.getElementById('25Id');
+    const _50Percent=document.getElementById('50Id');
+    const customId=document.getElementById('customId');
+    const errorId=document.getElementById('errorId');
+    const numberPeopleId=document.getElementById('numberPeopleId');
+    const quantityPersonId=document.getElementById('quantityPersonId')
+    const totalQuantityId=document.getElementById('totalQuantityId');
+});
